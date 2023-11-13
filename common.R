c16a2811b1d4dda0925d1e228479bdfb3f78dd6e
mdonertas <donertas.melike@gmail.com>
2023-11-14 00:06:03 +0100

    initial commit

---
+++ common.R
@@ -0,0 +1,12 @@
+library(tidyverse) # Includes ggplot2 + other data wrangling packages
+library(ggpubr) # Publication-ready figures & ggpubr theme
+library(ggthemes) # Additional themes for ggplot2
+library(viridis) # Viridis color palette
+library(colorspace) # For color manipulation
+library(ggforce) # Additional geoms
+library(hexbin) # For hexbin plots
+library(ggridges) # For creating ridgeline plots
+library(gridExtra) # Helps in arranging ggplot2 graphs (inset graphs)
+library(GGally) # Includes ggpairs and other useful functions
+
+theme_set(theme_bw(base_size = 10))
